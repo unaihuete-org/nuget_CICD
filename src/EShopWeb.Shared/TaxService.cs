a1e16ebf4a206d7e4bd1d9c353f282c1418deace
unaihuete93 <64772417+unaihuete93@users.noreply.github.com>
2022-09-08 12:48:00 +0000

    test feature branch

--- src/EShopWeb.Shared/TaxService.cs
+++ src/EShopWeb.Shared/TaxService.cs
@@ -3,6 +3,7 @@ public class TaxService
 {
     static public decimal CalculateTax(decimal taxable, string postalCode)
     {
+        // COMMENT change for CI testing
         return taxable * (decimal).1;
     }
 
